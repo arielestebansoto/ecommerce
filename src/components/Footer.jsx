d3300cb4a8120dd383c24ef0f8db0c2455a6766b
ariel soto <arielestebansoto@gmail.com>
2021-07-29 00:06:49 -0300

    add Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Footer = () => (
+    <footer className="page-footer blue darken-4">
+        <div className="container">
+            <div className="row">
+                <div className="col s12 center">
+                    <h5 className="white-text">Links</h5>
+                    <ul>
+                        <li><span className="grey-text text-lighten-3">About NOCommerce</span></li>
+                        <li><span className="grey-text text-lighten-3">Work with us</span></li>
+                        <li><span className="grey-text text-lighten-3">Customer Support</span></li>
+                        <li><span className="grey-text text-lighten-3">About the creator</span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div className="footer-copyright center">
+            <div className="container">
+                © 2050 Copyright NOCommerce S.R.L.
+            </div>
+        </div>
+  </footer>
+)
+
+export default Footer
