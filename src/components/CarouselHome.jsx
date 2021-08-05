f08cc1d006a618f7c67c7e27ab960db4916798c1
Ariel Esteban Soto <arielestebansoto@gmail.com>
2021-08-05 12:32:05 -0300

    debugin the rendering of images

--- src/components/CarouselHome.jsx
+++ src/components/CarouselHome.jsx
@@ -8,24 +8,52 @@ import carousel3 from '../assets/pictures/img/carouselhome/carousel-3.png'
 import carousel4 from '../assets/pictures/img/carouselhome/carousel-4.png'
 import carousel5 from '../assets/pictures/img/carouselhome/carousel-5.png'
 
-const CarouselHome = () => {
-    document.addEventListener('DOMContentLoaded', function() {
-        var elems = document.querySelectorAll('.CarouselHome');
-        var instances = M.Carousel.init(elems, {
-            fullWidth: true,
-            indicators: true,
-        });
-      });
+class CarouselHome extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            load: false,
+        }
+        this.carouselInit = this.carouselInit.bind(this)
+    }
+    componentDidMount() {
+        console.log('mount')
+        this.carouselInit()
+        this.setState({
+                load: true
+        }, 
+        this.carouselInit()
+        )
+    }
+    componentWillUnmount() {
+        console.log('unmount')
+    }
+    carouselInit() {
+        document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelectorAll('.CarouselHome');
+            var instances = M.Carousel.init(elems, {
+                fullWidth: true,
+                indicators: true,
+            });
+            });
+    }
 
-    return (
-    <div className="carousel CarouselHome carousel-slider">
-        <a className="carousel-item Carousel-item " href="#one!"><img src={carousel1} /></a>
-        <a className="carousel-item Carousel-item" href="#two!"><img src={carousel2} /></a>
-        <a className="carousel-item Carousel-item" href="#three!"><img src={carousel3} /></a>
-        <a className="carousel-item Carousel-item" href="#four!"><img src={carousel4} /></a>
-        <a className="carousel-item Carousel-item" href="#five!"><img src={carousel5} /></a>
-    </div>
-            
-    )
+    render() {
+        if (this.state.load === false) {
+            return <h1>Loading...</h1>
+        }
+        if (this.state.load === true){
+                return (
+                    <div className="carousel CarouselHome carousel-slider">
+                        <a className="carousel-item Carousel-item " href="#1"><img src={carousel1} /></a>
+                        <a className="carousel-item Carousel-item" href="#2"><img src={carousel2} /></a>
+                        <a className="carousel-item Carousel-item" href="#3"><img src={carousel3} /></a>
+                        <a className="carousel-item Carousel-item" href="#4"><img src={carousel4} /></a>
+                        <a className="carousel-item Carousel-item" href="#5"><img src={carousel5} /></a>
+                        <a className="carousel-item Carousel-item" href="#6"><img src={carousel3} /></a>
+                    </div>
+                )
+            }
+        } 
 }
 export default CarouselHome
