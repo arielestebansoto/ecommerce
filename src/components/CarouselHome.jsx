2a00ab79861fa36d0a0ce4ec22043f5801125eb8
Ariel Esteban Soto <arielestebansoto@gmail.com>
2021-08-07 12:17:46 -0300

    Refactoring to Class Component and some tests more, not working anyway

--- src/components/CarouselHome.jsx
+++ src/components/CarouselHome.jsx
@@ -11,49 +11,28 @@ import carousel5 from '../assets/pictures/img/carouselhome/carousel-5.png'
 class CarouselHome extends React.Component {
     constructor() {
         super()
-        this.state = {
-            load: false,
-        }
-        this.carouselInit = this.carouselInit.bind(this)
     }
     componentDidMount() {
-        console.log('mount')
-        this.carouselInit()
-        this.setState({
-                load: true
-        }, 
-        this.carouselInit()
-        )
-    }
-    componentWillUnmount() {
-        console.log('unmount')
-    }
-    carouselInit() {
         document.addEventListener('DOMContentLoaded', function() {
             var elems = document.querySelectorAll('.CarouselHome');
             var instances = M.Carousel.init(elems, {
                 fullWidth: true,
                 indicators: true,
             });
-            });
+        });
     }
 
     render() {
-        if (this.state.load === false) {
-            return <h1>Loading...</h1>
-        }
-        if (this.state.load === true){
-                return (
-                    <div className="carousel CarouselHome carousel-slider">
-                        <a className="carousel-item Carousel-item " href="#1"><img src={carousel1} /></a>
-                        <a className="carousel-item Carousel-item" href="#2"><img src={carousel2} /></a>
-                        <a className="carousel-item Carousel-item" href="#3"><img src={carousel3} /></a>
-                        <a className="carousel-item Carousel-item" href="#4"><img src={carousel4} /></a>
-                        <a className="carousel-item Carousel-item" href="#5"><img src={carousel5} /></a>
-                        <a className="carousel-item Carousel-item" href="#6"><img src={carousel3} /></a>
-                    </div>
-                )
-            }
-        } 
+        return (
+            <div className="carousel CarouselHome carousel-slider">
+                <a className="carousel-item Carousel-item " href="#1"><img src={carousel1} /></a>
+                <a className="carousel-item Carousel-item" href="#2"><img src={carousel2} /></a>
+                <a className="carousel-item Carousel-item" href="#3"><img src={carousel3} /></a>
+                <a className="carousel-item Carousel-item" href="#4"><img src={carousel4} /></a>
+                <a className="carousel-item Carousel-item" href="#5"><img src={carousel5} /></a>
+                <a className="carousel-item Carousel-item" href="#6"><img src={carousel3} /></a>
+            </div>
+        )
+    } 
 }
 export default CarouselHome
