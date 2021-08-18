42ada713c6fb73e54b7a71be029094ff76fec95b
Ariel Esteban Soto <arielestebansoto@gmail.com>
2021-08-18 16:30:25 -0300

    refactoring Product to class component

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import ProductStar from '../components/ProductStar'
 import ProductImageGalery from '../components/ProductImageGalery'
@@ -6,20 +7,30 @@ import PurchaseDetails from '../components/PurchaseDetails'
 import ProductDetails from '../components/ProductDetails'
 import ProductQuestion from '../components/ProductQuestion'
 import Footer from '../components/Footer'
+import productsReducer from '../reducers/productsReducer'
 
-const Product = (props) => {
-    return (
-        <div>
-            <div className="container">
-                <h5>Product Title</h5>
-                <ProductStar />
-                <ProductImageGalery />
-                <PurchaseDetails />
-                <ProductDetails />
-                <ProductQuestion />
+class Product extends React.Component {
+    
+    render() {
+        console.log(this.props)
+        return (
+            <div>
+                <h4>Product: { this.props.match.params.id }</h4>
+                <div className="container">
+                    <h5>Product Title</h5>
+                    <ProductStar />
+                    <ProductImageGalery />
+                    <PurchaseDetails />
+                    <ProductDetails />
+                    <ProductQuestion />
+                </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
-    )
+        )
+    }
+} 
+
+const mapStateToProps = ({productsReducer}) => {
+    return productsReducer
 }
-export default Product
+export default connect(mapStateToProps)(Product)
