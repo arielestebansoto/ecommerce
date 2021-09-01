ca57b31e016c17b8e265df0337d960aae0dfcba7
Ariel Esteban Soto <arielestebansoto@gmail.com>
2021-09-01 17:13:43 -0300

    input color modified, refacctoring styles of CardShopping Cart

--- src/components/CardShoppingCart.jsx
+++ src/components/CardShoppingCart.jsx
@@ -2,46 +2,25 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { removeProductFromCart } from '../actions/shoppingCartActions'
 
+import '../assets/styles/components/CardShoppingCart.scss'
+
 const CardShoppingCart = (props) => {
     const { product, cart } = props
     const handleClick = () => props.removeProductFromCart(product, cart) 
     return (
-    <div className="row z-depth-2" style={{paddingBlockStart: "16px"}}>
+    <div className="row z-depth-2 CardShoppingCart">
         <div className="col s12 m7">
-            <div className="Item" style={{display: "flex", marginBlockEnd: "24px"}}>
-                <img src={ product.image }
-                    style={{
-                        blockSize: "60px",
-                        inlineSize: "60px",
-                    }}
-                />
-                <div className="CardContent" style={{
-                    paddingInlineStart: "24px", 
-                    inlineSize: "100%"
-                    }}>
-                    <h6 style={{marginBlockEnd: "16px"}}>{ product.title }</h6>
-                    <div className="CardDetails" style={{
-                        display: "flex", 
-                        justifyContent: "space-between", 
-                        alignItems: "center"
-                        }}>
-
-                        {/* <div style={{
-                            border: "1px solid rgba(0,0,0, .3)",
-                            borderRadius: "5px",
-                            inlineSize: "30px",
-                            blockSize: "30px",
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}>  
-                        </div> */}
-                            <span><b>$ { product.price }</b></span>
-                        </div>
+            <div className="Item CardShoppingCart_item">
+                <img src={ product.image }/>
+                <div className="CardContent">
+                    <h6>{ product.title }</h6>
+                    <div className="CardDetails">
+                        <span><b>$ { product.price }</b></span>
                     </div>
+                </div>
             </div>
             <div className="divider"></div>
-            <div className="ItemAcions"style={{padding: "8px"}}>
+            <div className="ItemAcions">
                 <button 
                     className="btn-flat blue-text darken-4"
                     onClick={handleClick}
