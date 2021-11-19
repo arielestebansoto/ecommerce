f2ca0d1220637e87238e6f9755a162cd1d649bd5
Ariel Esteban Soto <arielestebansoto@gmail.com>
2021-11-19 15:24:10 -0300

    loader in the center of the page

--- src/components/global/Loader.jsx
+++ src/components/global/Loader.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 import '../../assets/styles/components/global/Loader.scss'
 
-const Loader = () => <div className="lds-default"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+const Loader = () => 
+<div className="lds-default">
+    <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+</div>
 
 export default Loader
